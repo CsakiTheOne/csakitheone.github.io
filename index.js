9b3c2784ce77ccfe44e69feb516065427179ad02
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2021-04-10 14:28:49 +0200

    Cleaned content

--- index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('btnNavigate').addEventListener('click', () => {
-    var date = document.getElementById('dateNavigate').value.split('-');
-    window.location = './sites/' + date[1] + '/' + date[2];
-});

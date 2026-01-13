a68f3f5d2c418ed33abe09338a952d7840c56907
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-01-13 16:13:27 +0100

    Finally have an index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,65 @@
 <script>
     import { setRandomSplashText } from "$lib/splash";
     import { onMount } from "svelte";
+    import logoKolora from "$lib/media/logos/kolora.png";
+    import logoWholesomeWare from "$lib/media/logos/wholesomeware.png";
+    import logoMoriForgatos from "$lib/media/logos/mori-forgatos.jpg";
 
     onMount(() => {
         setRandomSplashText();
     });
-
 </script>
 
-<h1>🚧 Under construction... 🚧</h1>
+<div class="screen">
+    <a href="https://kolora.web.app">
+        <img src={logoKolora} alt="Kolora Logo" />
+    </a>
+    <a href="/wholesomeware">
+        <img src={logoWholesomeWare} alt="WholesomeWare Logo" />
+    </a>
+    <a href="https://moriforgatos.hu/">
+        <img src={logoMoriForgatos} alt="Mori Forgatos Logo" />
+    </a>
+</div>
+
+<style>
+    @keyframes animBackgroundPos {
+        0% {
+            background-position: 0% 0%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 0%;
+        }
+    }
+
+    .screen {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+        height: 100svh;
+        gap: 1rem;
+        padding: 1rem;
+        color: white;
+        background: linear-gradient(
+            -45deg,
+            #4558a4,
+            #d27b45,
+            #4558a4,
+            #931621,
+            #4558a4
+        );
+        background-size: 400% 400%;
+        animation: animBackgroundPos 20s infinite;
+    }
+
+    img {
+        width: 20vw;
+        max-width: 150px;
+        border-radius: 50%;
+        box-shadow: 0 0 16px rgba(0, 0, 0, 0.5);
+    }
+</style>
